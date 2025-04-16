b6ed6629c79f854d0b986ac386d5d53d8dc4d2ce
RoyaleYourDad <royaleyourdad@gmail.com>
2025-04-16 18:13:12 +0500

    ig this is the end of fixes...        but im sure now

--- app.js
+++ app.js
@@ -303,6 +303,6 @@ app.get('/database.json', (req, res) => {
   });
 });
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
